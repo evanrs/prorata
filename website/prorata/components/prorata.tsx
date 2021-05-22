a73d434f5488d1ffe1c066c86a55d1675c23e7fd
evanrs <me@evanrs.com>
2021-05-22 15:04:42 -0400

    feat: configure styled-jsx

---
+++ website/prorata/components/prorata.tsx
@@ -0,0 +1,14 @@
+export function Prorata(): JSX.Element {
+  return (
+    <div>
+      hello
+      <style jsx>{`
+        .container {
+          height: 10vh;
+          color: white;
+          background-color: #eee;
+        }
+      `}</style>
+    </div>
+  )
+}
