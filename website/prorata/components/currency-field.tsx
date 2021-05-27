3cef21b0d4fe65f9168e29b9ee5cb87ffc5c232b
evanrs <me@evanrs.com>
2021-05-27 05:42:49 -0400

    feat: manage caret position

--- website/prorata/components/currency-field.tsx
+++ website/prorata/components/currency-field.tsx
@@ -3,35 +3,70 @@ import { NumberInput, NumberInputField } from '@chakra-ui/react'
 import { useDebounce } from 'use-debounce'
 import currency from 'currency.js'
 
-import { useAutoFocus } from '../client'
+import { useLayoutEffect, useAutoFocus } from '../client'
 import { FieldProps, fieldStyleProps } from './field'
 
 type NumberProps = Parameters<typeof NumberInput>[0]
 
 export const CurrencyField: React.FC<NumberProps & FieldProps> = (props) => {
   const { name, value, set, autoFocus, ...rest } = props
+  const { ref } = useAutoFocus(autoFocus)
+
+  // Store the formatted value
   const [formatted, setFormatted] = useState(() => {
     return value ? format(value) : ''
   })
-  useEffect(() => setFormatted(value ? format(value) : ''), [value])
+  // Update the formatted value on prop change
+  useEffect(() => {
+    setFormatted(value ? format(value) : '')
+  }, [value])
 
-  const [nextValue, { flush }] = useDebounce(parse(formatted) ?? value, 1000)
+  // Debounce change events to not disrupt typing
+  const [nextValue, { flush }] = useDebounce(parse(formatted) ?? value, 300)
+  // Update ancestor with change
   useEffect(() => {
     if (set && value !== nextValue) {
       set(name, nextValue)
     }
   }, [nextValue])
 
+  // Store the caret position
+  const [caret, setCaret] = useState(formatted.length)
+  // Maintain the caret position within the input on change
+  useLayoutEffect(() => {
+    if (ref.current) {
+      console.log('layout', { caret })
+      ref.current.selectionStart = ref.current.selectionEnd = caret
+    }
+  }, [caret])
+
   return (
     <NumberInput
-      {...useAutoFocus(autoFocus)}
+      inputMode="numeric"
       {...rest}
       name={name}
       value={formatted}
       onBlur={flush}
-      onChange={(raw: string) => setFormatted(format(raw))}
+      // onKeyPress={({ key }: { key: string }) => {
+      //   if (key === ',' || key === '.') {
+      //     setFormatted((f) => f + key)
+      //   }
+      // }}
+      onChange={(raw: string) => {
+        // remove all non-numeric characters
+        raw = raw.replace(/[^0-9]/g, '')
+
+        const next = format(raw)
+        const caret = ref.current?.selectionStart ?? 0
+        const diff = next.length - formatted.length > 1 ? 1 : 0
+
+        if (next !== formatted) {
+          setCaret(caret + diff)
+          setFormatted(next)
+        }
+      }}
     >
-      <NumberInputField {...fieldStyleProps} textAlign="right" />
+      <NumberInputField ref={ref} autoFocus={autoFocus} {...fieldStyleProps} textAlign="right" />
     </NumberInput>
   )
 }
