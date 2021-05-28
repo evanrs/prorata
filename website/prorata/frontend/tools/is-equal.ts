a66768cfe62a9b331e376972c0d1dc6ef6bcaec4
evanrs <me@evanrs.com>
2021-05-28 03:54:27 -0400

    fix: actually check for equality … use a library next time

--- website/prorata/frontend/tools/is-equal.ts
+++ website/prorata/frontend/tools/is-equal.ts
@@ -6,7 +6,7 @@ export function isEqual<T extends Record<string, unknown>>(
     return false
   }
 
-  return Object.keys(reference).every((v, k) => v === partial[k])
+  return Object.keys(reference).every((k) => reference[k] === partial[k])
 }
 
 export function isNotEqual<T extends Record<string, unknown>>(reference?: T, partial?: Partial<T>) {
